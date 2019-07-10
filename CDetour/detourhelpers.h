522f96f81e9e23f73fc5bc04084cfb37255f4d58
electr0w <electroshoc.m@gmail.com>
2019-07-10 21:24:11 +0500

    apply spumer seg fault fix patch, see details here: https://forums.alliedmods.net/showpost.php?p=2655750&postcount=15
    Signed-off-by: electr0w <electroshoc.m@gmail.com>

--- CDetour/detourhelpers.h
+++ CDetour/detourhelpers.h
@@ -8,7 +8,7 @@
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, version 3.0, as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -32,11 +32,19 @@
 #ifndef _INCLUDE_SOURCEMOD_DETOURHELPERS_H_
 #define _INCLUDE_SOURCEMOD_DETOURHELPERS_H_
 
+//#define DETOURHELPERS_LOG(...) L4D_DEBUG_LOG(__VA_ARGS__)
+#define DETOURHELPERS_LOG(...)
+
 #if defined PLATFORM_LINUX
 #include <sys/mman.h>
 #define	PAGE_SIZE	4096
 #define ALIGN(ar) ((long)ar & ~(PAGE_SIZE-1))
 #define	PAGE_EXECUTE_READWRITE	PROT_READ|PROT_WRITE|PROT_EXEC
+#else
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
 #endif
 
 struct patch_t
@@ -52,9 +60,22 @@ struct patch_t
 
 inline void ProtectMemory(void *addr, int length, int prot)
 {
+	DETOURHELPERS_LOG("Protecting memory...");
+
 #if defined PLATFORM_LINUX
 	void *addr2 = (void *)ALIGN(addr);
+	void *addr3 = (void*)ALIGN((unsigned char*)((unsigned char*)addr + length));
 	mprotect(addr2, sysconf(_SC_PAGESIZE), prot);
+
+    /* sometimes our [addr, addr+patch->bytes] may span more than one code page
+	   this is fairly rare. thankfully since our patch->bytes is at most 20 ,
+	   it will span at most 2 code pages
+	 */
+	if( addr3 != addr2)
+	{
+		DETOURHELPERS_LOG("Protecting memory across 2 code pages...");
+		mprotect(addr3, sysconf(_SC_PAGESIZE), prot);
+	}
 #elif defined PLATFORM_WINDOWS
 	DWORD old_prot;
 	VirtualProtect(addr, length, prot, &old_prot);
@@ -77,9 +98,10 @@ inline void DoGatePatch(unsigned char *target, void *callback)
 
 inline void ApplyPatch(void *address, int offset, const patch_t *patch, patch_t *restore)
 {
-	ProtectMemory(address, 20, PAGE_EXECUTE_READWRITE);
-
 	unsigned char *addr = (unsigned char *)address + offset;
+
+	ProtectMemory((void *)addr, patch->bytes, PAGE_EXECUTE_READWRITE);
+
 	if (restore)
 	{
 		for (size_t i=0; i<patch->bytes; i++)
@@ -89,8 +111,10 @@ inline void ApplyPatch(void *address, int offset, const patch_t *patch, patch_t 
 		restore->bytes = patch->bytes;
 	}
 
+	DETOURHELPERS_LOG("Copying new patch into memory...");
 	for (size_t i=0; i<patch->bytes; i++)
 	{
+		DETOURHELPERS_LOG("Copying new patch into memory byte %d/%d...", i+1, patch->bytes);
 		addr[i] = patch->patch[i];
 	}
 }
